1b37527a0e8e4f032b915811ae994b138a69b188
41833202@qq.com <41833202@qq.com>
2018-12-24 23:03:34 +0800

    Initial commit

---
+++ common/models.py
@@ -0,0 +1,118 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+
+from common.utils import COUNTRIES, ROLES
+import time
+
+
+def img_url(self, filename):
+    hash_ = int(time.time())
+    return "%s/%s/%s" % ("profile_pics", hash_, filename)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    file_prepend = "users/profile_pics"
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(('date joined'), auto_now_add=True)
+    role = models.CharField(max_length=50, choices=ROLES)
+    profile_pic = models.FileField(max_length=1000, upload_to=img_url, null=True, blank=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', ]
+
+    objects = UserManager()
+
+    def get_short_name(self):
+        return self.username
+
+    def __unicode__(self):
+        return self.email
+
+
+class Address(models.Model):
+    address_line = models.CharField(_("Address"), max_length=255, blank=True, null=True)
+    street = models.CharField(_("Street"), max_length=55, blank=True, null=True)
+    city = models.CharField(_("City"), max_length=255, blank=True, null=True)
+    state = models.CharField(_("State"), max_length=255, blank=True, null=True)
+    postcode = models.CharField(_("Post/Zip-code"), max_length=64, blank=True, null=True)
+    country = models.CharField(max_length=3, choices=COUNTRIES, blank=True, null=True)
+
+    def __str__(self):
+        return self.city if self.city else ""
+
+    def get_complete_address(self):
+        address = ""
+        if self.address_line:
+            address += self.address_line
+        if self.street:
+            if address:
+                address += ", " + self.street
+            else:
+                address += self.street
+        if self.city:
+            if address:
+                address += ", " + self.city
+            else:
+                address += self.city
+        if self.state:
+            if address:
+                address += ", " + self.state
+            else:
+                address += self.state
+        if self.postcode:
+            if address:
+                address += ", " + self.postcode
+            else:
+                address += self.postcode
+        if self.country:
+            if address:
+                address += ", " + self.get_country_display()
+            else:
+                address += self.get_country_display()
+        return address
+
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=55)
+    members = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    case = models.ForeignKey('cases.Case', blank=True, null=True, related_name="cases", on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255)
+    commented_on = models.DateTimeField(auto_now_add=True)
+    commented_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    account = models.ForeignKey(
+        'accounts.Account', blank=True, null=True, related_name="accounts_comments", on_delete=models.CASCADE)
+    lead = models.ForeignKey('leads.Lead', blank=True, null=True, related_name="leads", on_delete=models.CASCADE)
+    opportunity = models.ForeignKey(
+        'opportunity.Opportunity', blank=True, null=True, related_name="opportunity_comments", on_delete=models.CASCADE)
+    contact = models.ForeignKey(
+        'contacts.Contact', blank=True, null=True, related_name="contact_comments", on_delete=models.CASCADE)
+
+    def get_files(self):
+        return Comment_Files.objects.filter(comment_id=self)
+
+
+class Comment_Files(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    comment_file = models.FileField("File", upload_to="comment_files", default='')
+
+    def get_file_name(self):
+        if self.comment_file:
+            return self.comment_file.path.split('/')[-1]
+        else:
+            return None
---
+++ common/views.py
@@ -0,0 +1,240 @@
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import (
+    CreateView, UpdateView, DetailView, TemplateView, View, DeleteView)
+from common.models import User
+from common.forms import UserForm, LoginForm, ChangePasswordForm, PasswordResetEmailForm
+from django.contrib.auth.views import PasswordResetView
+from django.urls import reverse_lazy
+from django.conf import settings
+
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def handler500(request):
+    return render(request, '500.html', status=500)
+
+
+class AdminRequiredMixin(AccessMixin):
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        self.raise_exception = True
+        if not request.user.role == "ADMIN":
+            if not request.user.is_superuser:
+                return self.handle_no_permission()
+        return super(AdminRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+
+class HomeView(LoginRequiredMixin, TemplateView):
+
+    template_name = "index.html"
+
+
+class ChangePasswordView(LoginRequiredMixin, TemplateView):
+    template_name = "change_password.html"
+
+    def post(self, request, *args, **kwargs):
+        error, errors = "", ""
+        form = ChangePasswordForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            if not check_password(request.POST.get('CurrentPassword'), user.password):
+                error = "Invalid old password"
+            else:
+                user.set_password(request.POST.get('Newpassword'))
+                user.is_active = True
+                user.save()
+                return HttpResponseRedirect('/')
+        else:
+            errors = form.errors
+        return render(request, "change_password.html", {'error': error, 'errors': errors})
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        context["user_obj"] = self.request.user
+        return context
+
+
+class LoginView(TemplateView):
+    template_name = "login.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect('/')
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST, request=request)
+        if form.is_valid():
+            user = authenticate(username=request.POST.get('email'), password=request.POST.get('password'))
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    return render(request, "login.html", {
+                        "error": True,
+                        "message": "Your Account is InActive. Please Contact Administrator"
+                    })
+            else:
+                return render(request, "login.html", {
+                    "error": True,
+                    "message": "Your Account is not Found. Please Contact Administrator"
+                })
+        else:
+            return render(request, "login.html", {
+                "error": True,
+                "message": "Your username and password didn't match. Please try again."
+            })
+
+
+class ForgotPasswordView(TemplateView):
+    template_name = "forgot_password.html"
+
+
+class LogoutView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        request.session.flush()
+        return redirect("common:login")
+
+
+class UsersListView(AdminRequiredMixin, TemplateView):
+    model = User
+    context_object_name = "users"
+    template_name = "list.html"
+
+    def get_queryset(self):
+        queryset = self.model.objects.all()
+        request_post = self.request.POST
+        if request_post:
+            if request_post.get('first_name'):
+                queryset = queryset.filter(first_name__icontains=request_post.get('first_name'))
+            if request_post.get('last_name'):
+                queryset = queryset.filter(last_name_id=request_post.get('last_name'))
+            if request_post.get('username'):
+                queryset = queryset.filter(username__icontains=request_post.get('username'))
+            if request_post.get('email'):
+                queryset = queryset.filter(email__icontains=request_post.get('email'))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(UsersListView, self).get_context_data(**kwargs)
+        context["users"] = self.get_queryset()
+        context["active_users"] = self.get_queryset().filter(is_active=True)
+        context["inactive_users"] = self.get_queryset().filter(is_active=False)
+        context["per_page"] = self.request.POST.get('per_page')
+        context['admin_email'] = settings.ADMIN_EMAIL
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
+
+
+class CreateUserView(AdminRequiredMixin, CreateView):
+    model = User
+    form_class = UserForm
+    template_name = "create.html"
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        if form.cleaned_data.get("password"):
+            user.set_password(form.cleaned_data.get("password"))
+        user.save()
+        if self.request.is_ajax():
+            data = {'success_url': reverse_lazy('common:users_list'), 'error': False}
+            return JsonResponse(data)
+        return super(CreateUserView, self).form_valid(form)
+
+
+    def form_invalid(self, form):
+        response = super(CreateUserView, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse({'error': True, 'errors': form.errors})
+        return response
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateUserView, self).get_context_data(**kwargs)
+        context["user_form"] = context["form"]
+        if "errors" in kwargs:
+            context["errors"] = kwargs["errors"]
+        return context
+
+
+class UserDetailView(AdminRequiredMixin, DetailView):
+    model = User
+    context_object_name = "users"
+    template_name = "list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        users_list = User.objects.all()
+        context.update({
+            "users": users_list, "user_obj": self.object,
+            "active_users": users_list.filter(is_active=True),
+            "inactive_users": users_list.filter(is_active=False)
+        })
+        return context
+
+
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = "create.html"
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        if user.role == "USER":
+            user.is_superuser = False
+        user.save()
+        if self.request.user.role == "ADMIN" or self.request.user.is_superuser:
+            if self.request.is_ajax():
+                data = {'success_url': reverse_lazy('common:users_list'), 'error': False}
+                return JsonResponse(data)
+        if self.request.is_ajax():
+            data = {'success_url': reverse_lazy('common:profile'), 'error': False}
+            return JsonResponse(data)
+        return super(UpdateUserView, self).form_valid(form)
+
+
+    def form_invalid(self, form):
+        response = super(UpdateUserView, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse({'error': True, 'errors': form.errors})
+        return response
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdateUserView, self).get_context_data(**kwargs)
+        context["user_obj"] = self.object
+        context["user_form"] = context["form"]
+        if "errors" in kwargs:
+            context["errors"] = kwargs["errors"]
+        return context
+
+
+class UserDeleteView(AdminRequiredMixin, DeleteView):
+    model = User
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return redirect("common:users_list")
+
+
+class PasswordResetView(PasswordResetView):
+    template_name = 'registration/password_reset_form.html'
+    form_class = PasswordResetEmailForm
+    email_template_name = 'registration/password_reset_email.html'
